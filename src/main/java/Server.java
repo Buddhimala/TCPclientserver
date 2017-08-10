7a66d2fde61aba5a73244d345f33ebb2a4537a82
Buddhimala <rmbuddhimala@gmail.com>
2017-08-10 23:36:03 +0530

    Update Server.java

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,5 +1,4 @@
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.LinkedList;
@@ -7,106 +6,109 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Server {
-    private static ServerSocket srvsoc=null;
-    private static Socket clsoc=null;
-    private static final int maxclients=50;
-    public static final clientThread[] threads=new clientThread[maxclients];
-    public static final Queue<Socket> queue = new LinkedList<Socket>();
-    public static void main(String[] args) {
-        int port=2000;
-        if (args.length < 1) {
-            System.out.println("Usage: java MultiThreadChatServer <portNumber>\n"
-                    + "Now using port number=" + port);
-        } else {
-            port = Integer.valueOf(args[0]).intValue();
-        }
+  private static ServerSocket srvsoc = null;
+  private static Socket clsoc = null;
+  private static final int maxclients = 50;
+  static final Clientthread[] threads = new Clientthread[maxclients];
+  public static final Queue<Socket> queue = new LinkedList<Socket>();
 
-        try{
-            srvsoc=new ServerSocket(port); //port number on which server socket is binded
-        }catch(IOException ioex){
-            System.out.println("Unable to connect to port");
-            System.exit(1);
-        }
-        /*create a client socket for each connection and pass it to a new client thread*/
-        while(true){
-            try{
-                clsoc=srvsoc.accept(); //to accept the input from srvsocket
-                int i=0;
-                for(i=0;i<maxclients;i++){
-                    if(threads[i]==null){
-                        (threads[i]=new clientThread(clsoc,threads)).start();
-                        break;
-                    }
-                }if(i==maxclients){
-                    PrintStream p = new PrintStream(clsoc.getOutputStream());
-                    p.println("Server too busy. Try later.");
-                    clsoc.close();
-                }
-            }catch(IOException e){
-                e.printStackTrace();
-            }
-        }
+  public static void main(String[] args) {
+    int port = 2000;
+    if (args.length < 1) {
+      System.out.println("Usage: java MultiThreadServer <portNumber>\n"
+                        + "Now using port number=" + port);
+    } else {
+      port = Integer.valueOf(args[0]).parseInt(" ");
     }
+    try {
+      srvsoc = new ServerSocket(port); //port number on which server socket is binded
+    } catch (IOException ioex) {
+      System.out.println("Unable to connect to port");
+      System.exit(1);
+    }
+    /*create a client socket for each connection and pass it to a new client thread*/
+    while (true) {
+      try {
+        clsoc = srvsoc.accept(); //to accept the input from srvsocket
+        int i = 0;
+        for (i = 0;i < maxclients;i++) {
+          if (threads[i] == null) {
+            (threads[i] = new Clientthread(clsoc,threads)).start();
+            break;
+          }
+        }
+        if (i == maxclients) {
+          DataOutputStream p = new DataOutputStream(clsoc.getOutputStream());
+          p.writeChars("Server too busy. Try later.");
+          clsoc.close();
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 
 }
 
-class clientThread extends Thread{
-    private Scanner input=null;
-    private PrintStream p=null;
-    private Socket clsoc=null;
-    private final clientThread[] threads;
-    private int maxclients,number1,number2;
+class Clientthread extends Thread {
+  private Scanner input = null;
+  private PrintStream ps = null;
+  private Socket clsoc = null;
+  private final Clientthread[] threads;
+  private int maxclients;
+  private int number1;
+  private int number2;
 
-    public clientThread(Socket clsoc,clientThread[] threads){
-        this.clsoc=clsoc;
-        this.threads=threads;
-        maxclients=threads.length;
-    }
-    public void run(){
-        int maxclients=this.maxclients;
-        clientThread[] threads=this.threads;
-        Server newserver=new Server();
+  public Clientthread(Socket clsoc, Clientthread[] threads) {
+    this.clsoc = clsoc;
+    this.threads = threads;
+    maxclients = threads.length;
+  }
 
-        try{
-            input=new Scanner(clsoc.getInputStream()); //recieve input stream from the client
-            p=new PrintStream(clsoc.getOutputStream()); //Pass server response to the client
-            number1=input.nextInt();
-            number2=input.nextInt();
+  public void run() {
+    int maxclients = this.maxclients;
+    Clientthread[] threads = this.threads;
+    //Server newserver = new Server();
 
-            while (number1 != 0 && number2 != 0){
-                int temp=number1+number2;
-                p.println(temp);
-                number1=input.nextInt();
-                number2=input.nextInt();
-            }
-            //clean up the thread so that another task can be assigned to it
-            synchronized (this) {
-                for (int i = 0; i < maxclients; i++) {
-                    if (threads[i] == this) {
-                        threads[i] = null;
-                    }
-                }
-            }
-            if(newserver.queue.isEmpty()==false) {
-                clsoc = newserver.queue.peek();
-                for (int i = 0; i < maxclients; i++) {
-                    if (threads[i] == null) {
-                        (threads[i] = new clientThread(clsoc, Server.threads)).start();
-                        break;
-                    }
-                }
-            }
-        }catch(IOException e){
-            e.printStackTrace();
+    try {
+      input = new Scanner(clsoc.getInputStream()); //recieve input stream from the client
+      ps = new PrintStream(clsoc.getOutputStream()); //Pass server response to the client
+      number1 = input.nextInt();
+      number2 = input.nextInt();
+
+      while (number1 != 0 && number2 != 0) {
+        int temp = number1 + number2;
+        ps.println(temp);
+        number1 = input.nextInt();
+        number2 = input.nextInt();
+      }
+      //clean up the thread so that another task can be assigned to it
+      synchronized (this) {
+        for (int i = 0; i < maxclients; i++) {
+          if (threads[i] == this) {
+            threads[i] = null;
+          }
         }
-        finally{
-            try{
-                System.out.println("Closing connection ...");
-                clsoc.close();
-            }catch(IOException ie){
-                System.out.println("Unable to close connection");
-                System.exit(1);
-            }
+      }
+      if (Server.queue.isEmpty() == false) {
+        clsoc = Server.queue.peek();
+        for (int i = 0; i < maxclients; i++) {
+          if (threads[i] == null) {
+            (threads[i] = new Clientthread(clsoc, Server.threads)).start();
+            break;
+          }
         }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        System.out.println("Closing connection ...");
+        clsoc.close();
+      } catch (IOException ie) {
+        System.out.println("Unable to close connection");
+        //System.exit(1);
+      }
     }
+  }
 }
